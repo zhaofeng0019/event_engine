684c85f76ad11fcaab0b58473398bec10deb5cf4
xiaoff <zhaofeng_0019@163.com>
2021-09-10 20:44:07 -0700

    add errno

--- util.cpp
+++ util.cpp
@@ -3,26 +3,31 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <string>
+#include <errno.h>
 
 namespace event_engine
 {
 	int OpenPerfEvent(perf_event_attr *attr, int pid, int cpu, int group_fd, unsigned long flags)
 	{
-		return syscall(SYS_perf_event_open, attr, pid, cpu, group_fd, flags);
+		int res = syscall(SYS_perf_event_open, attr, pid, cpu, group_fd, flags);
+		return res < 0 ? errno : res;
 	}
 
 	int EnablePerfEvent(int fd)
 	{
-		return ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
+		int res = ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
+		return res < 0 ? errno : res;
 	}
 
 	int DisablePerfEvent(int fd)
 	{
-		return ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
+		int res = ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
+		return res < 0 ? errno : res;
 	}
 
 	int SetPerfFilter(int fd, std::string filter)
 	{
-		return ioctl(fd, PERF_EVENT_IOC_SET_FILTER, filter.c_str());
+		int res = ioctl(fd, PERF_EVENT_IOC_SET_FILTER, filter.c_str());
+		return res < 0 ? errno : res;
 	}
 }
