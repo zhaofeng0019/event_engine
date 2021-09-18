afa346b2c0e52358099a2f5187bdac083b089fe0
xiaoff <zhaofeng_0019@163.com>
2021-09-18 17:11:17 +0800

    modify monitor

--- monitor.cpp
+++ monitor.cpp
@@ -4,6 +4,9 @@
 #include <errno.h>
 #include <cstring>
 #include <fcntl.h>
+#include <chrono>
+#include <iostream>
+#include <cstdio>
 namespace event_engine
 {
     const int ATTR_SIZE{64};
@@ -20,8 +23,15 @@ namespace event_engine
             /* cgroup */
             if (option.perf_event_dir_ == "")
             {
-                option.perf_event_dir_ = TracingDir();
+                option.perf_event_dir_ = PerfEventDir();
             }
+
+            if (option.perf_event_dir_ == "")
+            {
+                err = "no perf event dir";
+                goto fail;
+            }
+
             for (auto cgroup : option.cgroup_)
             {
                 std::string path = option.perf_event_dir_ + "/" + cgroup;
@@ -38,6 +48,12 @@ namespace event_engine
             }
             return;
         }
+
+        if (option.cgroup_all_)
+        {
+            option.pids_.clear();
+            option.pids_.push_back(-1);
+        }
         /* pid */
         for (auto pid : option.pids_)
         {
@@ -48,22 +64,12 @@ namespace event_engine
         }
         return;
     fail:
-        if (err != "")
+        if (err == "")
         {
-            err = strerror(errno);
+            err = errno == 0 ? "monitor init fail" : strerror(errno);
         }
         ClearCgroupAndGroupFd();
         return;
-    }
-
-    std::unordered_map<uint64_t, Decoder> Monitor::GetDecoderMap()
-    {
-        return decoder_map_;
-    }
-
-    std::unordered_map<uint64_t, perf_event_attr> Monitor::GetAttrMap()
-    {
-        return attr_map_;
     }
 
     bool Monitor::Start(std::string &err)
@@ -112,12 +118,13 @@ namespace event_engine
         return is_running_;
     }
 
-    bool Monitor::InitGroupLeaders(int pid_or_fd, bool is_fd, unsigned long flag, int ring_buffer_page, std::string &err)
+    bool Monitor::InitGroupLeaders(int pid_or_fd, bool is_fd, unsigned long flag, uint32_t ring_buffer_page, std::string &err)
     {
         if (is_fd)
         {
             flag |= PERF_FLAG_PID_CGROUP;
         }
+
         perf_event_attr group_attr{0};
         group_attr.type = PERF_TYPE_SOFTWARE;
         group_attr.size = ATTR_SIZE;
@@ -132,32 +139,51 @@ namespace event_engine
             {
                 return false;
             }
-            group_fd_.push_back(group_fd);
+
+            group_.push_back(PerfEventGroup{
+                .pid_or_fd_ = pid_or_fd,
+                .group_fd_ = group_fd,
+                .flag_ = flag,
+                .cpu = cpu,
+            });
+
             RingBuffer ring_buffer(group_fd, ring_buffer_page);
             if (!ring_buffer.Mmap(err))
             {
                 return false;
             }
             ring_buffer_map_[group_fd] = ring_buffer;
+            if (!CpuTimeOffset(cpu, group_fd, ring_buffer, err))
+            {
+                return false;
+            }
         }
+        return true;
     }
 
     void Monitor::ClearCgroupAndGroupFd()
     {
-        for (auto fd : group_fd_)
+        for (auto group : group_)
         {
-            close(fd);
+            close(group.group_fd_);
         }
+
+        group_.clear();
+
         for (auto fd : cgroup_fd_)
         {
             close(fd);
         }
+
+        cgroup_fd_.clear();
         for (auto it : ring_buffer_map_)
         {
             it.second.Unmap();
         }
+        ring_buffer_map_.clear();
     }
-    uint64_t Monitor::CpuTimeOffset(int cpu, int group_fd, RingBuffer &ring_buffer, std::string &err)
+
+    bool Monitor::CpuTimeOffset(int cpu, int group_fd, RingBuffer &ring_buffer, std::string &err)
     {
         perf_event_attr refer_attr{0};
         refer_attr.type = PERF_TYPE_SOFTWARE;
@@ -196,21 +222,198 @@ namespace event_engine
             }
             PerfSampleRecord record;
             int offset{0};
-            if (!record.Read(buffs[0].second, buffs[0].first, offset, attr_map_, &refer_attr, decoder_map_))
+            if (!record.Read(buffs[0].second, buffs[0].first, offset, stream_id_attr_map_, &refer_attr, stream_id_decoder_map_))
             {
                 err = "read refer sample err";
                 goto fail;
             }
-
+            uint64_t time_offset = std::chrono::system_clock::now().time_since_epoch().count() - ring_buffer.TimeRunning() - record.time_;
+            ring_buffer.SetTimeOffset(offset);
             for (auto buff : buffs)
             {
                 delete[] buff.second;
             }
+            break;
         }
-
+        close(fd);
+        return true;
     fail:
         close(fd);
+        return false;
+    }
+
+    bool Monitor::EnableProbe(uint64_t probe_id, std::string &err)
+    {
+        std::lock_guard<std::mutex> lock(lock_);
+        auto it = probe_id_fd_map_.find(probe_id);
+        if (it == probe_id_fd_map_.end())
+        {
+            char buff[64]{0};
+            std::sprintf(buff, "probe_id %d no found", probe_id);
+            err = std::string(buff);
+            return false;
+        }
+        for (auto fd : it->second)
+        {
+            if (!EnablePerfEvent(fd, err))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool Monitor::DisableProbe(uint64_t probe_id, std::string &err)
+    {
+        std::lock_guard<std::mutex> lock(lock_);
+        auto it = probe_id_fd_map_.find(probe_id);
+        if (it == probe_id_fd_map_.end())
+        {
+            char buff[64]{0};
+            std::sprintf(buff, "probe_id %d no found", probe_id);
+            err = std::string(buff);
+            return false;
+        }
+        for (auto fd : it->second)
+        {
+            if (!DisablePerfEvent(fd, err))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool Monitor::EnableAll(std::string &err)
+    {
+        std::lock_guard<std::mutex> lock(lock_);
+        for (auto it : probe_id_fd_map_)
+        {
+            if (!EnableProbe(it.first, err))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool Monitor::DisableAll(std::string &err)
+    {
+        std::lock_guard<std::mutex> lock(lock_);
+        for (auto it : probe_id_fd_map_)
+        {
+            if (!DisableProbe(it.first, err))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    uint64_t Monitor::RegisterProbeEvent(ProbeOption &option, std::string &err)
+    {
+        std::lock_guard<std::mutex> lock(lock_);
+        if (option.is_dynamic_)
+        {
+            if (!AddProbe(option.group_, option.name_, option.address_, option.output_, option.on_return_, option.is_kprobe_, err))
+            {
+                return 0;
+            }
+        }
+        int event_id = GetTraceEventID(option.group_, option.name_, err);
+        if (event_id < 0)
+        {
+            return 0;
+        }
+
+        perf_event_attr attr{0};
+        if (option.attr_ != nullptr)
+        {
+            attr = *option.attr_;
+        }
+
+        attr.config = event_id;
+        attr.disabled = option.diabled_;
+        attr.type = PERF_TYPE_TRACEPOINT;
+        attr.size = ATTR_SIZE;
+        attr.sample_period = 1;
+        attr.sample_type |= PERF_SAMPLE_STREAM_ID | PERF_SAMPLE_IDENTIFIER | PERF_SAMPLE_TIME;
+        attr.pinned = false;
+        attr.freq = false;
+        attr.watermark = true;
+        attr.use_clockid = false;
+        attr.wakeup_watermark = 1;
+
+        std::vector<int> event_fds;
+        std::vector<uint64_t> stream_ids;
+
+        for (auto group : group_)
+        {
+            int fd = OpenPerfEvent(&attr, group.pid_or_fd_, group.cpu, group.group_fd_, group.flag_ | (PERF_FLAG_FD_OUTPUT | PERF_FLAG_FD_NO_GROUP), err);
+            if (fd < 0)
+            {
+                goto fail;
+            }
+            event_fds.push_back(fd);
+            uint64_t stream_id = GetStreamId(fd, err);
+            if (err != "")
+            {
+                goto fail;
+            }
+            stream_ids.push_back(stream_id);
+        }
+
+        probe_id_fd_map_[next_probe_id_] = event_fds;
+        probe_id_stream_id_map_[next_probe_id_] = stream_ids;
+        probe_id_dyamic_probe_map_[next_probe_id_] = std::tuple<std::string, std::string, bool>{option.group_, option.name_, option.on_return_};
+        return next_probe_id_++;
+
+    fail:
+        for (auto fd : event_fds)
+        {
+            close(fd);
+        }
+        if (option.is_dynamic_)
+        {
+            std::string void_err;
+            RemoveProbe(option.group_, option.name_, option.is_kprobe_, void_err);
+        }
         return 0;
     }
 
+    bool Monitor::RemoveProbeEvent(uint64_t probe_id, std::string &err)
+    {
+        std::lock_guard<std::mutex> lock(lock_);
+        char buff[64]{0};
+        auto it = probe_id_fd_map_.find(probe_id);
+        if (it == probe_id_fd_map_.end())
+        {
+            std::sprintf(buff, "probe_id %d no found", probe_id);
+            err = std::string(buff);
+            return false;
+        }
+        for (auto fd : it->second)
+        {
+            close(fd);
+        }
+
+        for (auto stream_id : probe_id_stream_id_map_[probe_id])
+        {
+            stream_id_decoder_map_.erase(stream_id);
+            stream_id_decoder_map_.erase(stream_id);
+        }
+
+        auto dynamic_it = probe_id_dyamic_probe_map_.find(probe_id);
+        if (dynamic_it == probe_id_dyamic_probe_map_.end())
+        {
+            return true;
+        }
+        auto dynamic_tuple = dynamic_it->second;
+        int res = RemoveProbe(std::get<0>(dynamic_tuple), std::get<1>(dynamic_tuple), std::get<2>(dynamic_tuple), err);
+        if (res < 0)
+        {
+            return false;
+        }
+        return true;
+    }
 }
--- monitor.h
+++ monitor.h
@@ -16,10 +16,19 @@ namespace event_engine
     struct MonitorOption
     {
         unsigned long flag_{0};
-        uint ring_buff_page_{0};
+        uint32_t ring_buff_page_{0};
         std::vector<int> pids_;
         std::string perf_event_dir_;
         std::vector<std::string> cgroup_;
+        bool cgroup_all_{false};
+    };
+
+    struct PerfEventGroup
+    {
+        int pid_or_fd_{0};
+        int group_fd_{0};
+        int cpu{0};
+        unsigned long flag_{0};
     };
 
     struct ProbeOption
@@ -29,8 +38,10 @@ namespace event_engine
         std::string filter_;
         std::string address_;
         std::string output_;
+        bool diabled_{true};
+        bool on_return_{false};
         std::function<void(void *)> decoder_handle_{nullptr};
-        perf_event_attr *attr_;
+        perf_event_attr *attr_{nullptr};
         bool is_dynamic_{false};
         bool is_kprobe_{false};
     };
@@ -39,18 +50,18 @@ namespace event_engine
     {
     private:
         std::mutex lock_;
-        uint64_t next_probe_id_{1};                                                                 /*protect by lock_*/
-        std::unordered_map<uint64_t, Decoder> decoder_map_;                                         /*protect by lock_*/
-        std::unordered_map<uint64_t, perf_event_attr> attr_map_;                                    /* protect by lock_ */
-        std::unordered_map<uint64_t, std::vector<uint64_t>> probe_id_stream_id_map_;                /*protect by lock_*/
-        std::unordered_map<uint64_t, std::tuple<std::string, std::string, bool>> dyamic_probe_map_; /*protect by lock_*/
-        std::unordered_map<uint64_t, std::vector<int>> probe_id_cgroup_fd_map_;                     /* protect by lock_ */
-        std::vector<int> group_fd_;
+        uint64_t next_probe_id_{1};                                                                          /*protect by lock_*/
+        std::unordered_map<uint64_t, Decoder> stream_id_decoder_map_;                                        /*protect by lock_*/
+        std::unordered_map<uint64_t, perf_event_attr> stream_id_attr_map_;                                   /* protect by lock_ */
+        std::unordered_map<uint64_t, std::vector<int>> probe_id_fd_map_;                                /*protect by lock_*/
+        std::unordered_map<uint64_t, std::vector<uint64_t>> probe_id_stream_id_map_;                         /*protect by lock_*/
+        std::unordered_map<uint64_t, std::tuple<std::string, std::string, bool>> probe_id_dyamic_probe_map_; /*protect by lock_*/
+
+        std::vector<PerfEventGroup> group_;
         std::vector<int> cgroup_fd_;
         std::unordered_map<int, RingBuffer> ring_buffer_map_;
 
         pollfd *poll_fd_{nullptr};
-
         bool is_running_{false};
 
         int cpus_{0};
@@ -58,21 +69,23 @@ namespace event_engine
         int done_fd_{0};
 
         void RunLoop(void);
-        bool InitGroupLeaders(int pid_fd, bool is_fd, unsigned long flag, int ring_buffer_page, std::string &err);
+        bool InitGroupLeaders(int pid_fd, bool is_fd, unsigned long flag, uint32_t ring_buffer_page, std::string &err);
         void ClearCgroupAndGroupFd();
-        uint64_t CpuTimeOffset(int cpu, int group_fd, RingBuffer &ring_buffer, std::string &err);
+        bool CpuTimeOffset(int cpu, int group_fd, RingBuffer &ring_buffer, std::string &err);
 
     public:
         bool IsRunning();
         bool Start(std::string &err);
         void Stop();
-        uint64_t AddProbe(ProbeOption &option, std::string &err);
-        void RemoveProbe(uint64_t probe_id);
         Monitor(MonitorOption &option, std::string &err);
         Monitor(Monitor &) = delete;
         Monitor &operator=(Monitor &) = delete;
-        // std::unordered_map<uint64_t, Decoder> GetDecoderMap();
-        // std::unordered_map<uint64_t, perf_event_attr> GetAttrMap();
+        bool EnableProbe(uint64_t probe_id, std::string &err);
+        bool EnableAll(std::string &err);
+        bool DisableProbe(uint64_t probe_id, std::string &err);
+        bool DisableAll(std::string &err);
+        uint64_t RegisterProbeEvent(ProbeOption &option, std::string &err);
+        bool RemoveProbeEvent(uint64_t probe_id, std::string &err);
     };
 }
 
