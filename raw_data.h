c241c685067f69c490b3a53ac95af520233bf3a1
xiaoff <zhaofeng_0019@163.com>
2021-09-20 22:33:16 -0700

    replace vector by map for convenience

--- raw_data.h
+++ raw_data.h
@@ -1,20 +1,19 @@
 #ifndef EVENT_ENGINE_RAW_DATA_INC_
 #define EVENT_ENGINE_RAW_DATA_INC_
-#include <vector>
+#include <unordered_map>
 #include <string>
 #include "trace_event_field.h"
 namespace event_engine
 {
     struct FieldData
     {
-        std::string name_;
-        int size_{0};
-        char *ptr_{nullptr};
+        uint32_t size_{0};   /* size of field value*/
+        char *ptr_{nullptr}; /* ptr of field ;should not use out of decorder_handle function */
     };
     class RawData
     {
     public:
-        std::vector<FieldData> fields_;
+        std::unordered_map<std::string, FieldData> field_map_;
         void Parse(std::vector<TraceEventField> &trace_fields, char *data_ptr, const int total_size);
     };
 }
